bee12917ba47f390cbb77b88bcca1f10786db6c2
Mark23Dev <sarunimark06@gmail.com>
2025-08-07 10:10:07 +0300

    Added ORDER BY query to the left join in joins_queries.sql

--- .history/database-adv-script/joins_queries_20250807100835.sql
+++ .history/database-adv-script/joins_queries_20250807100835.sql
@@ -39,3 +39,15 @@ SELECT
 FROM
     users
 FULL OUTER JOIN bookings ON users.id = bookings.user_id;
+
+-- Retrieve all properties and their reviews, including properties that have no reviews
+SELECT 
+    properties.id AS property_id,
+    properties.name AS property_name,
+    reviews.id AS review_id,
+    reviews.rating,
+    reviews.comment
+FROM 
+    properties
+LEFT JOIN 
+    reviews ON properties.id = reviews.property_id
